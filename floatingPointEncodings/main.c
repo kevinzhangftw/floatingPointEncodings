f2b5711bec8549538ba497e1c8dbbd2c07a08516
Kai Zhang <kwz@asb9700-e06.csil.sfu.ca>
2016-01-25 14:45:07 -0800

    Initial Commit

---
+++ floatingPointEncodings/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  floatingPointEncodings
+//
+//  Created by Kai Zhang on 2016-01-25.
+//  Copyright © 2016 Kai Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
